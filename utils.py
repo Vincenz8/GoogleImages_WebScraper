7d8a02967f3ced162f43240eaa4e8822a489bb68
Vincenzo <vincenzo.russ8@outlook.com>
2022-06-03 15:47:52 +0200

    start with vscode

--- utils.py
+++ utils.py
@@ -3,6 +3,6 @@ import json
 
 
 def load_config(filename: str) -> dict:
-    with open(file= filename, mode="r") as json_file:
+    with open(file=filename, mode="r") as json_file:
         config = json.load(json_file)
     return config
