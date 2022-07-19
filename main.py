b5dbfcca6a7352c1673bc25159268a7b0f00376a
Vincenzo <vincenzo.russ8@outlook.com>
2022-07-19 12:35:51 +0200

    pivoted to chrome

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # 3rd party libraries
 from selenium import webdriver
-from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.chrome.service import Service
 
 # local libraries
 from utils import load_config
@@ -9,15 +9,19 @@ SCRAPER_CONFIG = "data/scraper_config.json"
 
 
 def main():
-    gecko_driver = Service(executable_path="data/geckodriver")
-    driver = webdriver.Firefox(service=gecko_driver)
+    chrome_driver = Service(executable_path="data/chromedriver")
+    driver = webdriver.Chrome(service=chrome_driver)
     scraper_config = load_config(filename=SCRAPER_CONFIG)
 
     for b_style in scraper_config["bonsai_styles"]:
-        driver.get(url=f"https://duckduckgo.com/?t=ffab&q={b_style}+bonsai&iax=images&ia=images")
-        driver.implicitly_wait(time_to_wait=3)
-
-    driver.quit()
+        url = f"https://www.google.com/search?q={b_style}+bonsai&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiHnMa83YT5AhWCXvEDHTM-AaoQ_AUoAXoECAEQAw&biw=1366&bih=668"
+        driver.get(url=url)
+         # agree cookies
+        xpath_accept_button = '//*[@id="L2AGLb"]/div'
+        driver.find_element(by="xpath", value=xpath_accept_button).click()
+        driver.implicitly_wait(time_to_wait=50)
+        break
+    
 
 
 if __name__ == '__main__':
